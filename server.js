104caa9508ccb8b2df52e66834da42fba26e69c6
Aditi Tiwari <adititiwari1204@gmail.com>
2025-11-05 20:54:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
     res.send("Hello DevOps CI/CD Project Running Successfully!");
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0" , () => {
     console.log(`Server running on port ${port}`);
 });
 
